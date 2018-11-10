2efa546501b27e8a7f32859f56a74ef630fe22c7
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2018-11-10 21:44:14 +0300

    feat: get_subject capability

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ extern crate nom;
 use std::fs::File;
 use std::io::prelude::*;
 
+named!(take_until_break<&str, &str>, take_until!("//"));
+named!(take_until_subj<&str, &str>, take_until!("SUBJ/"));
+
 pub fn open(filename: &str) -> String {
     let mut f = File::open(filename).expect("File not found");
     let mut contents = String::new();
@@ -17,8 +20,7 @@ fn can_open_files() {
 }
 
 fn get_classification(contents: &str) -> &str {
-    named!(x<&str, &str>, take_until!("//"));
-    match x(contents) {
+    match take_until_break(contents) {
         Ok(i) => i.1,
         Err(_) => "UNKNOWN",
     }
@@ -57,4 +59,25 @@ fn can_get_header_classification() {
                     INFO CNO WASHINGTON DC
                     ";
     assert_eq!(get_header(contents).classification, "UNCLASSIFIED");
+}
+
+pub fn get_subject(contents: &str) -> &str {
+    named!(parse<&str, &str>, do_parse!(
+        take_until_subj >>
+        subject: take_until_break >>
+        (subject)
+    ));
+    match parse(contents) {
+        Ok(i) => &i.1[5..],
+        Err(_) => "UNKNOWN"
+    }
+}
+#[test]
+fn can_get_subject() {
+    let nav18255 = open("data/NAVADMIN/NAV18255.txt");
+    let subject18255 = get_subject(&nav18255);
+    assert_eq!(subject18255, "2018-2019 NAVY INFLUENZA VACCINATION AND REPORTING POLICY");
+    let nav18256 = open("data/NAVADMIN/NAV18256.txt");
+    let subject18256 = get_subject(&nav18256);
+    assert_eq!(subject18256, "FISCAL YEAR 2019 CYBERSECURITY AWARENESS CHALLENGE");
 }
